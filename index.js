0e3b458b9ba84432a7eb6c1cf3332a6838fbac80
Bhautik Navdariya <114481071+bhautikn@users.noreply.github.com>
2023-10-02 20:11:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,148 @@
+const http = require('http');
+const httpWsServer = http.createServer();
+const fs = require('fs');
+const io = require('socket.io')(httpWsServer, { cors: { origin: "*" } });
+
+
+const app = http.createServer((req, res)=>{
+    if(req.url === '/'){
+        let indexFile = fs.readFileSync('./public/index.html');
+        res.writeHead(200,  {'Content-Type': 'text/html'});
+        res.end(indexFile.toString());
+    }
+    else if(req.url === '/style.css'){
+        let indexFile = fs.readFileSync('./public/style.css');
+        res.writeHead(200,  {'Content-Type': 'text/css'});
+        res.end(indexFile.toString());
+    }
+    else if(req.url === '/script.js'){
+        let indexFile = fs.readFileSync('./public/script.js');
+        res.writeHead(200,  {'Content-Type': 'text/javascript'});
+        res.end(indexFile.toString());
+    }
+})
+
+
+let counter = 0;
+let clients = {};
+let clientsArray = [];
+let isAnyWinner = false;
+let arr = [];
+for (let i = 0; i < 9; i++)
+    arr.push('');
+
+function isAnyWin(){
+    for (let i=0;i<3;i++) {
+        if(arr[i] === arr[i+3] && arr[i+3] === arr[i+6] && arr[i] !== ""){
+            emitOwnTern(clients, clientsArray, false, false);
+            io.emit('win', 'player '+arr[i]+' is winner', [i, i+3, i+6])
+            return true;
+        }
+    }
+    for (let i=0;i<7;i=i+3) {
+            if(arr[i] === arr[i+1] && arr[i+1] === arr[i+2] && arr[i] !== ""){
+            emitOwnTern(clients, clientsArray, false, false);
+            io.emit('win', 'player '+arr[i]+' is winner', [i, i+1, i+2])
+            return true;
+        }
+    }
+    if(arr[0] === arr[4] && arr[4] === arr[8] && arr[0] !== ""){
+            emitOwnTern(clients, clientsArray, false, false);
+            io.emit('win', 'player '+arr[0]+' is winner', [0, 4, 8])
+            return true;       
+    }
+    if(arr[2] === arr[4] && arr[4] === arr[6] && arr[2] !== ""){
+            emitOwnTern(clients, clientsArray, false, false);
+            io.emit('win', 'player '+arr[2]+' is winner', [2, 4, 6])
+            return true;   
+    }
+    return false;
+}
+
+io.on('connection', socket => {
+    if (clientsArray.length < 2) {
+        let obj = {};
+        if (clientsArray.length === 0) {
+            obj[socket.id] = { isYourTurn: true, Symbol: 'x' };
+            io.to(socket.id).emit('whoistern', false);
+            io.to(socket.id).emit('WhatMySymbol', 'x');
+            io.to(socket.id).emit('notice', 'waiting for other player....')
+        }
+        else {
+            obj[socket.id] = { isYourTurn: false, Symbol: 'o' };
+            io.to(socket.id).emit('whoistern', false);
+            io.to(clientsArray[0]).emit('whoistern', true);
+            io.to(socket.id).emit('WhatMySymbol', 'o');
+            io.to(clientsArray[0]).emit('notice', 'Your Turn');
+            clients[clientsArray[0]].isYourTurn = true;
+        }
+        clientsArray.push(socket.id);
+        clients = { ...clients, ...obj };
+        console.log(clients);
+    }
+    socket.on('disconnect', ()=>{
+        io.emit('notice', 'other player is disconnected');
+        if(clientsArray[0] == socket.id){
+            clientsArray.splice(0,1);
+        }
+        else{
+            clientsArray.pop();
+        }
+        for (let i = 0; i < 9; i++)
+            arr[i] = '';
+        counter = 0;
+        io.to(clientsArray[0]).emit('whoistern', false);
+        io.to(clientsArray[0]).emit('WhatMySymbol', 'x');
+        io.to(clientsArray[0]).emit('notice', 'waiting for other player....')
+        io.to(clientsArray[0]).emit('clearDiv', '')
+        let obj = {};
+        obj[clientsArray[0]] = { isYourTurn: false, Symbol: 'x' }
+        clients = {...obj};
+
+    })
+    socket.on('changeOnDiv', (iterator) => {
+        if (arr[iterator] === "" && clients[socket.id].isYourTurn && counter < 9 && !isAnyWinner) {
+            arr[iterator] = clients[socket.id].Symbol;
+            io.emit('chngeOnDiv', arr[iterator], iterator);
+            counter++;
+            isAnyWinner = isAnyWin();
+            if(counter > 8){
+                io.emit('win', 'tie', [])
+                isAnyWinner = true;
+            }
+
+            if(!isAnyWinner){
+                if (counter % 2 == 0) {
+                    emitOwnTern(clients, clientsArray, true, false);
+                }
+                else
+                    emitOwnTern(clients, clientsArray, false, true);
+            }
+        }
+        if(isAnyWinner){
+            setTimeout(()=>{
+                isAnyWinner = false;
+                emitOwnTern(clients, clientsArray, true, false);
+                counter = 0;
+                for (let i = 0; i < 9; i++)
+                    arr[i] = '';
+            }, 5000);
+        }
+    });
+})
+
+function emitOwnTern(obj, arr, a, b) {
+    try{
+        obj[arr[0]].isYourTurn = a;
+        obj[arr[1]].isYourTurn = b;
+        io.to(arr[0]).emit('whoistern', a)
+        io.to(arr[1]).emit('whoistern', b)
+    }catch(e){
+        console.log(e);
+    }
+}
+
+httpWsServer.listen(8080)
+app.listen(3000, ()=>{
+    console.log('server started on port', 3000)
+});
---
+++ public/script.js
@@ -0,0 +1,60 @@
+const socket = io('ws://' + window.location.hostname + ':8080');
+let isMyturn = false;
+let gameDiv = document.getElementsByClassName('game-div');
+let counter = 0;
+let WhatMySymbol = '';
+
+socket.on('whoistern', whoseTern => {
+    isMyturn = whoseTern;
+    console.log(isMyturn);
+    if (isMyturn)
+        document.getElementById('whoistern').innerHTML = "Your Turn";
+    else
+        document.getElementById('whoistern').innerHTML = "Other player Turn";
+})
+socket.on('chngeOnDiv', (symbol, divId) => {
+    gameDiv[divId].innerHTML = symbol;
+})
+
+socket.on('clearDiv', () => {
+    for (const iterator of gameDiv) {
+        iterator.innerHTML = '';
+        iterator.style.color = 'black';
+    }
+})
+
+socket.on('WhatMySymbol', symbol => {
+    WhatMySymbol = symbol;
+    document.querySelector('.symbol').innerHTML = WhatMySymbol;
+})
+
+socket.on('win', (who, arr) => {
+    document.getElementById('whoistern').innerHTML = who;
+    for (let i of arr) {
+        gameDiv[i].style.color = 'red';
+    }
+    setTimeout(() => {
+        for (const it of gameDiv) {
+            it.innerHTML = "";
+            it.style.color = "black";
+        }
+        clearInterval(id1);
+        document.querySelector('h3').innerHTML = "";
+    }, 6000)
+    let second = 4;
+    let id1 = setInterval(() => {
+        document.querySelector('h3').innerHTML = 'Game restart in ' + second--;
+    }, 1000)
+
+})
+socket.on('notice', (notice) => {
+    document.getElementById('whoistern').innerHTML = notice;
+})
+for (let iterator = 0; iterator < gameDiv.length; iterator++) {
+    gameDiv[iterator].addEventListener('click', (e) => {
+        if (isMyturn && e.target.innerHTML === "") {
+            e.target.innerHTML = WhatMySymbol;
+            socket.emit('changeOnDiv', iterator);
+        }
+    })
+}
